258eb6bfe431678db07d20ad9207039c64f195ce
Alexander Ziegler <29373163+alexander-ziegler-simonsen@users.noreply.github.com>
2026-03-15 23:30:03 +0100

    client - added chakra provider, react-router and all the routes that is in the app for now

---
+++ client/src/components/ui/provider.tsx
@@ -0,0 +1,11 @@
+// import { ColorModeProvider } from "@/components/ui/color-mode"
+import { ChakraProvider, defaultSystem } from "@chakra-ui/react"
+
+export function Provider({ children }: { children: React.ReactNode }) {
+    return (
+        <ChakraProvider value={defaultSystem}>
+            {/* <ColorModeProvider {...props} /> */}
+            {children}
+        </ChakraProvider>
+    )
+}
