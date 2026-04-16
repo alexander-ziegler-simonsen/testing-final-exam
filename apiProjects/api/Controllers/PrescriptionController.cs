102caf6e4a2051419bbe8efb1e3a267fec676bf1
Alexander Ziegler <29373163+alexander-ziegler-simonsen@users.noreply.github.com>
2026-04-16 12:18:41 +0200

    api - added working prescription service, interface and controller

--- apiProjects/api/Controllers/PrescriptionController.cs
+++ apiProjects/api/Controllers/PrescriptionController.cs
@@ -1,5 +1,6 @@
 using hospitalApi.DTOs.Inputs;
 using hospitalApi.DTOs.Outputs;
+using hospitalApi.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace hospitalApi.Controllers
@@ -8,68 +9,66 @@ namespace hospitalApi.Controllers
     [ApiController]
     public class PrescriptionController : ControllerBase
     {
-        // private IPrescriptionService _PrescriptionService;
+        private IPrescriptionService _PrescriptionService;
 
-        // public PrescriptionController (IPrescriptionService PrescriptionService) {
-        //     _PrescriptionService = PrescriptionService;
-        // }
+        public PrescriptionController(IPrescriptionService PrescriptionService)
+        {
+            _PrescriptionService = PrescriptionService;
+        }
 
+        // GET: api/<PrescriptionController>
+        [HttpGet]
+        public async Task<IEnumerable<PrescriptionOutput>> GetAllPrescriptions()
+        {
+            var output = await _PrescriptionService.GetAll();
+            return output;
+        }
 
-        // // GET: api/<PrescriptionController>
-        // [HttpGet]
-        // public async Task<IEnumerable<PrescriptionOutput>> GetAllPrescriptions()
-        // {
-        //     var output = await _PrescriptionService.GetAll();
+        // GET api/<PrescriptionController>/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<PrescriptionOutput>> Get(int id)
+        {
+            PrescriptionOutput output = await _PrescriptionService.GetOne(id);
+            if (output == null)
+                return NotFound();
+            else
+                return Ok(output);
+        }
 
-        //     return output;
-        // }
+        // POST api/<PrescriptionController>
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] PrescriptionInput newPrescription)
+        {
+            bool output = await _PrescriptionService.CreatePrescription(newPrescription);
 
-        // // GET api/<PrescriptionController>/5
-        // [HttpGet("{id}")]
-        // public async Task<ActionResult<PrescriptionOutput>> Get(int id)
-        // {
-        //     PrescriptionOutput output = await _PrescriptionService.GetOne(id);
-        //     if (output == null)
-        //         return NotFound();
-        //     else
-        //         return Ok(output);
-        // }
+            if (output)
+                return Ok();
+            else
+                return NoContent();
+        }
 
-        // // POST api/<PrescriptionController>
-        // [HttpPost]
-        // public async Task<ActionResult> Post([FromBody] PrescriptionInput newPrescription)
-        // {
-        //     bool output = await _PrescriptionService.CreatePrescription(newPrescription);
+        // PUT api/<PrescriptionController>/5
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Put(int id, [FromBody] PrescriptionInput newPrescription)
+        {
+            bool output = await _PrescriptionService.EditPrescription(id, newPrescription);
 
-        //     if (output)
-        //         return Ok();
-        //     else
-        //         return NoContent();
-        // }
+            if (output)
+                return Ok();
+            else
+                return NoContent();
+        }
 
-        // // PUT api/<PrescriptionController>/5
-        // [HttpPut("{id}")]
-        // public async Task<ActionResult> Put(int id, [FromBody] PrescriptionInput newPrescription)
-        // {
-        //     bool output = await _PrescriptionService.EditPrescription(id, newPrescription);
+        // DELETE api/<PrescriptionController>/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            bool output = await _PrescriptionService.DeletePrescription(id);
 
-        //     if (output)
-        //         return Ok();
-        //     else
-        //         return NoContent();
-        // }
-
-        // // DELETE api/<PrescriptionController>/5
-        // [HttpDelete("{id}")]
-        // public async Task<ActionResult> Delete(int id)
-        // {
-        //     bool output = await _PrescriptionService.DeletePrescription(id);
-
-        //     if (output)
-        //         return Ok();
-        //     else
-        //         return NoContent();
-        // }
+            if (output)
+                return Ok();
+            else
+                return NoContent();
+        }
     }
-
 }
---
+++ apiProjects/api/Services/Interfaces/IPrescriptionService.cs
@@ -0,0 +1,14 @@
+using hospitalApi.DTOs.Inputs;
+using hospitalApi.DTOs.Outputs;
+
+namespace hospitalApi.Services.Interfaces
+{
+    public interface IPrescriptionService
+    {
+        Task<IEnumerable<PrescriptionOutput>> GetAll();
+        Task<PrescriptionOutput> GetOne(int id);
+        Task<bool> EditPrescription(int PrescriptionId, PrescriptionInput editedPrescriptionData);
+        Task<bool> DeletePrescription(int id);
+        Task<bool> CreatePrescription(PrescriptionInput newPrescription);
+    }
+}
