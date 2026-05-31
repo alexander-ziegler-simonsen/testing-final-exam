af7676e47fc04cb1180974a7c46f9ff1a6787692
Alexander Ziegler <29373163+alexander-ziegler-simonsen@users.noreply.github.com>
2026-05-31 19:47:08 +0200

    fixed e2e config

--- tests/e2e/playwright.config.ts
+++ tests/e2e/playwright.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig, devices } from '@playwright/test';
+import path from 'path';
 
 /**
  * Read environment variables from file.
@@ -67,10 +68,11 @@ export default defineConfig({
     // },
   ],
 
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'pnpm run dev',
+    url: 'http://localhost:5173',
+    reuseExistingServer: !process.env.CI,
+    cwd: path.join(__dirname, '../../client'),
+    timeout: 60_000,
+  },
 });
