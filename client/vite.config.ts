1dfd5c0dd1baec8ce50817c71e273b05373585f2
Alexander Ziegler <29373163+alexander-ziegler-simonsen@users.noreply.github.com>
2026-05-31 15:11:27 +0200

    streamline dockerfiles to build client and api, which runs inside my compose file

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,8 +3,16 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true,
+    strictPort: true,
+    port: 5173,
+  },
   test: {
-    environment: 'node',
+    environment: 'happy-dom',
     globals: true,
     setupFiles: ['./src/setupTests.ts'],
   },
