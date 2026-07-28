0fe04b77bcc8953fbbe15ea5eb9c24c87b01f160
Alexander Ziegler <29373163+alexander-ziegler-simonsen@users.noreply.github.com>
2026-07-28 14:50:09 +0200

    added a reuseble style, that can be used on all chraka components

---
+++ client/src/components/CustomRecipes.tsx
@@ -0,0 +1,8 @@
+import { defineStyle } from "@chakra-ui/react"
+
+
+export const quickShadow = defineStyle({
+    shadow: "0 0 6px 1px rgba(0, 0, 0, 0.5)",
+    transition: "box-shadow 0.1s cubic-bezier(0.16, 1, 0.3, 1)"
+})
+
