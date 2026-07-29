7e5e546779fc8ba4ce0cc715ec0da33a25114f7e
Alexander Ziegler <29373163+alexander-ziegler-simonsen@users.noreply.github.com>
2026-07-29 18:38:22 +0200

    moved the timeline over to shifts

--- client/src/pages/dashboards/Shifts.tsx
+++ client/src/pages/dashboards/Shifts.tsx
@@ -1,8 +1,14 @@
+import { OverlappingTimeline } from "../../components/dashboards/OverlappingTimeLine";
+import { FixedTimelineGrid } from "../../components/dashboards/SimpleTimeline";
 
 export default function Shifts() {
   return (
     <>
       <p>this is Shifts page</p>
+
+      <FixedTimelineGrid />
+      <br /><hr />
+      <OverlappingTimeline />
     </>
   );
 }
