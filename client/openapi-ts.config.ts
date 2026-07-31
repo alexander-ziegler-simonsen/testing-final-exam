eca12638690a4c1b17cfe775d75dad62c8802774
Alexander Ziegler <29373163+alexander-ziegler-simonsen@users.noreply.github.com>
2026-07-31 10:32:02 +0200

    changed the "hey api" config file to use the newely created swagger file

--- client/openapi-ts.config.ts
+++ client/openapi-ts.config.ts
@@ -2,13 +2,11 @@ import { defineConfig } from "@hey-api/openapi-ts";
 
 export default defineConfig({
     // Direct file link within your local monorepo workspaces
-    input: "../apiProjects/api/docs/openapi.yaml",
+    input: "../apiProjects/api/swagger/v1/swagger.json",
     output: "src/api",
-    plugins: [
-        "@hey-api/client-axios", 
+    plugins: [ 
         "zod",
-        {
-            name: "@hey-api/sdk",
-        },
+        { name: "@hey-api/sdk" },
+        { name: "@hey-api/client-axios" }
     ],
 });
