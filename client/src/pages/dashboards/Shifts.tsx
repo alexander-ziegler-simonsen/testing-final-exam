5e93f79a395b6f3fd7741fac5018fa2e38550efb
Alexander Ziegler <29373163+alexander-ziegler-simonsen@users.noreply.github.com>
2026-08-03 11:39:08 +0200

    updated shift to use the services

--- client/src/pages/dashboards/Shifts.tsx
+++ client/src/pages/dashboards/Shifts.tsx
@@ -1,14 +1,22 @@
+import { useEffect, useState } from "react";
 import { OverlappingTimeline } from "../../components/dashboards/OverlappingTimeLine";
-import { FixedTimelineGrid } from "../../components/dashboards/SimpleTimeline";
+import type { HospitalApiDtosOutputsShiftOutputDto } from "../../api";
+import { ShiftService } from "../../services/Shift";
 
 export default function Shifts() {
+  const [data, setData] = useState<HospitalApiDtosOutputsShiftOutputDto[]>([]);
+
+  useEffect(() => {
+    ShiftService.getAll()
+      .then(setData)
+      .catch((error) => console.error(error));
+  }, []);
+
   return (
     <>
       <p>this is Shifts page</p>
 
-      <FixedTimelineGrid />
-      <br /><hr />
-      <OverlappingTimeline />
+      <OverlappingTimeline data={data} />
     </>
   );
 }
