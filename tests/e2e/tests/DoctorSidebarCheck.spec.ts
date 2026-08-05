8effd82329eeaf93966a7d5be373dcb3c8f1cdfa
Alexander Ziegler <29373163+alexander-ziegler-simonsen@users.noreply.github.com>
2026-08-05 23:14:33 +0200

    added more e2e tests

---
+++ tests/e2e/tests/DoctorSidebarCheck.spec.ts
@@ -0,0 +1,27 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('http://localhost:5173/');
+  await page.getByTestId('public-footer-login-link').click();
+  await page.goto('http://localhost:5173/login');
+
+  await page.getByTestId('login-username-input').fill('doctor');
+  await page.getByTestId('login-password-input').fill('Doctor1234!');
+  await page.getByTestId('login-submit-button').click();
+
+  // check that the sidebar links are visible
+  await page.goto('http://localhost:5173/app/overview');
+  await expect.soft(page.getByTestId('sidebar-desktop-departments-link-open-button')).toBeVisible();
+  await expect.soft(page.getByTestId('sidebar-desktop-department-staff-link-open-button')).toBeVisible();
+  await expect.soft(page.getByTestId('sidebar-desktop-facilities-link-open-button')).toBeVisible();
+  await expect.soft(page.getByTestId('sidebar-desktop-missing-medicin-link-open-button')).toBeVisible();
+  await expect.soft(page.getByTestId('sidebar-desktop-medicin-storage-link-open-button')).toBeVisible();
+  await expect.soft(page.getByTestId('sidebar-desktop-external-medicin-link-open-button')).toBeVisible();
+  await expect.soft(page.getByTestId('sidebar-desktop-patients-link-open-button')).toBeVisible();
+  await expect.soft(page.getByTestId('sidebar-desktop-shifts-link-open-button')).toBeVisible();
+  await expect.soft(page.getByTestId('sidebar-desktop-room-booking-link-open-button')).toBeVisible();
+  await expect.soft(page.getByTestId('sidebar-desktop-treatment-link-open-button')).toBeVisible();
+
+  await page.getByTestId('dashboard-navbar-logout-button').click();
+  await page.goto('http://localhost:5173/login');
+});
---
+++ tests/e2e/tests/doctorDepartments.spec.ts
@@ -0,0 +1,68 @@
+import { test, expect } from "@playwright/test";
+
+test("test", async ({ page }) => {
+  await page.goto("http://localhost:5173/");
+  await page.getByTestId("public-footer-login-link").click();
+  await page.getByTestId("login-username-input").click();
+  await page.goto("http://localhost:5173/login");
+
+  await page.getByTestId("login-username-input").fill("doctor");
+  await page.getByTestId("login-password-input").fill("Doctor1234!");
+  await page.getByTestId("login-submit-button").click();
+
+  await page.goto("http://localhost:5173/app/overview");
+  await page.getByTestId("sidebar-desktop-departments-link-open-button").click();
+
+  await page.goto("http://localhost:5173/app/departments");
+
+  // read all data
+  await expect(page.getByTestId("departments-table-row-0-cell-id")).toContainText("1");
+  await expect(page.getByTestId("departments-table-row-0-cell-name")).toContainText("emergency");
+  await expect(page.getByTestId("departments-table-row-0-cell-type")).toContainText("critical care");
+
+  await expect(page.getByTestId("departments-table-row-1-cell-id")).toContainText("2");
+  await expect(page.getByTestId("departments-table-row-1-cell-name")).toContainText("surgery");
+  await expect(page.getByTestId("departments-table-row-1-cell-type")).toContainText("operation");
+
+  await expect(page.getByTestId("departments-table-row-2-cell-id")).toContainText("3");
+  await expect(page.getByTestId("departments-table-row-2-cell-name")).toContainText("cardiology");
+  await expect(page.getByTestId("departments-table-row-2-cell-type")).toContainText("specialist");
+
+  await expect(page.getByTestId("departments-table-row-3-cell-id")).toContainText("4");
+  await expect(page.getByTestId("departments-table-row-3-cell-name")).toContainText("pediatrics");
+  await expect(page.getByTestId("departments-table-row-3-cell-type")).toContainText("child care");
+
+  await expect(page.getByTestId("departments-table-row-4-cell-id")).toContainText("5");
+  await expect(page.getByTestId("departments-table-row-4-cell-name")).toContainText("radiology");
+  await expect(page.getByTestId("departments-table-row-4-cell-type")).toContainText("diagnostics");
+
+  // edit one
+  await page.getByTestId('departments-edit-1').click();
+  await page.getByTestId('departments-form-field-name').fill('emergency1');
+  await page.getByTestId('departments-form-field-type').fill('critical care1');
+  await page.getByTestId('departments-form-submit-button').click();
+
+  // check that it got updated
+  await expect(page.getByTestId('departments-table-row-4-cell-id')).toContainText('1');
+  await expect(page.getByTestId('departments-table-row-4-cell-name')).toContainText('emergency1');
+  await expect(page.getByTestId('departments-table-row-4-cell-type')).toContainText('critical care1');
+  
+  // add a new one
+  await page.getByTestId('departments-add-button').click();
+  await page.getByTestId('departments-form-field-name').fill('test2');
+  await page.getByTestId('departments-form-field-type').fill('test2');
+  await page.getByTestId('departments-form-submit-button').click();
+
+  // check that it got added
+  await expect(page.getByTestId('departments-table-row-5-cell-id')).toContainText('6');
+  await expect(page.getByTestId('departments-table-row-5-cell-name')).toContainText('test2');
+  await expect(page.getByTestId('departments-table-row-5-cell-type')).toContainText('test2');
+  
+  // delete it
+  await page.getByTestId('departments-delete-6').click();
+  await page.getByTestId('departments-form-submit-button').click();
+
+  // logout
+  await page.getByTestId('dashboard-navbar-logout-button').click();
+  await page.goto("http://localhost:5173/login");
+});
