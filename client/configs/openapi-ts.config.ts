2f3c880fdba418f08550fd6a4dc903b199565d15
Alexander Ziegler <29373163+alexander-ziegler-simonsen@users.noreply.github.com>
2026-08-05 10:42:56 +0200

    updated all the other files, to reference the right location

--- client/configs/openapi-ts.config.ts
+++ client/configs/openapi-ts.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "@hey-api/openapi-ts";
 
 export default defineConfig({
-    input: "../apiProjects/api/swagger/v1/swagger.json",
+    input: "../api/swagger/v1/swagger.json",
     output: "src/api",
     plugins: [ 
         { name: 'zod', includeInEntry: true },
