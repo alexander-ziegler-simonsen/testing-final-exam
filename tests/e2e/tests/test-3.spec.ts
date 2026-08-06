8120c7fa018cf7101a718dbc71e5f030e64fc0cd
Alexander Ziegler <29373163+alexander-ziegler-simonsen@users.noreply.github.com>
2026-08-06 10:25:31 +0200

    updates to all the other e2e tests

--- tests/e2e/tests/test-3.spec.ts
+++ tests/e2e/tests/test-3.spec.ts
@@ -1,4 +1,9 @@
 import { test, expect } from '@playwright/test';
+import { resetDb } from './dbReset';
+
+test.beforeEach(async () => {
+  await resetDb();
+});
 
 test('test', async ({ page }) => {
   await page.goto('http://localhost:5173/');
