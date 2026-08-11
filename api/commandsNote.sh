2849da9a9bdf91dbff3b2e24750a8e7200a0b07b
Alexander Ziegler <29373163+alexander-ziegler-simonsen@users.noreply.github.com>
2026-08-11 11:43:48 +0200

    added better commands for api

--- api/commandsNote.sh
+++ api/commandsNote.sh
@@ -7,3 +7,8 @@ dotnet run
 # to force a new openAPI file - one of the 2
 dotnet build --no-incremental
 dotnet clean && dotnet build
+dotnet clean -p api/hospitalApi.csproj && dotnet build -p api/hospitalApi.csproj
+
+
+# do this one in the root folder
+dotnet clean -p api && dotnet build -p api
