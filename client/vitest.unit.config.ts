5fe34b106f0389a860d0b30269836fd887bca401
Alexander Ziegler <29373163+alexander-ziegler-simonsen@users.noreply.github.com>
2026-08-12 16:11:47 +0200

    updated unit vitest config to use jsdom

--- client/vitest.unit.config.ts
+++ client/vitest.unit.config.ts
@@ -4,7 +4,8 @@ import { defineProject } from 'vitest/config'
 export default defineProject({
     test: {
         name: 'unit',
-        environment: 'node',
+        // environment: 'node',
+        environment: "jsdom",
         include: ['src/**/*.unit.test.ts'],
         setupFiles: [path.resolve(__dirname, './src/mocks/setupMswNode.ts')],
     },
